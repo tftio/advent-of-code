e4e905cc6e464b0a73a534708d28a82158b22a6d
James Felix Black <jfb@homonculus.net>
2021-11-30 10:27:58 -0500

    Create the project

---
+++ README.md
@@ -0,0 +1,6 @@
+# advent-of-code
+
+[![Hackage](https://img.shields.io/hackage/v/advent-of-code.svg?logo=haskell)](https://hackage.haskell.org/package/advent-of-code)
+[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+See README for more info
---
+++ src/AdventOfCode.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 James Felix Black
+SPDX-License-Identifier: MIT
+Maintainer: James Felix Black <github@homonculus.net>
+
+See README for more info
+-}
+
+module AdventOfCode
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
